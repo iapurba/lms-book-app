ccaabd3306f06e1309f39935cdf7fc33b939de0c
Apurba Panja <apurbapanja1@gmail.com>
2024-09-18 10:56:41 +0530

    Initial commit for lms-book-app

---
+++ src/test/java/com/iapurba/lms_book_app/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iapurba.lms_book_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
