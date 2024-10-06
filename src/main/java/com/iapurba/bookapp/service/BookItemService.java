bcd0f58f2b51ca5b62dd5c0234256438be0fccdb
Apurba Panja <apurbapanja1@gmail.com>
2024-10-06 15:38:25 +0530

    Remove redundant modifiers

--- src/main/java/com/iapurba/bookapp/service/BookItemService.java
+++ src/main/java/com/iapurba/bookapp/service/BookItemService.java
@@ -2,10 +2,9 @@ package com.iapurba.bookapp.service;
 
 import com.iapurba.bookapp.dto.BookItemRequestDto;
 import com.iapurba.bookapp.dto.BookItemResponseDto;
-import com.iapurba.bookapp.model.entity.BookItem;
 
 public interface BookItemService {
-    public BookItemResponseDto createUpdateBookItem(BookItemRequestDto bookItemDto) throws Exception;
-    public BookItemResponseDto getBookItemById(Long id) throws Exception;
-    public void deleteBookItemById(Long id) throws Exception;
+    BookItemResponseDto createUpdateBookItem(BookItemRequestDto bookItemDto) throws Exception;
+    BookItemResponseDto getBookItemById(Long id) throws Exception;
+    void deleteBookItemById(Long id) throws Exception;
 }
--- src/main/java/com/iapurba/bookapp/service/BookService.java
+++ src/main/java/com/iapurba/bookapp/service/BookService.java
@@ -1,13 +1,10 @@
 package com.iapurba.bookapp.service;
 
 import com.iapurba.bookapp.dto.BookDto;
-import com.iapurba.bookapp.model.entity.Book;
-
-import java.util.Optional;
 
 public interface BookService {
-    public BookDto createBook(BookDto bookDto) throws Exception;
-    public BookDto updateBook(String isbn, BookDto bookDto) throws Exception;
-    public BookDto getBookByIsbn(String isbn) throws Exception;
-    public void deleteBookByIsbn(String isbn) throws Exception;
+    BookDto createBook(BookDto bookDto) throws Exception;
+    BookDto updateBook(String isbn, BookDto bookDto) throws Exception;
+    BookDto getBookByIsbn(String isbn) throws Exception;
+    void deleteBookByIsbn(String isbn) throws Exception;
 }
